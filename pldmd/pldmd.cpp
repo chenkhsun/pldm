b53badcd0f527a2b449bdacaa0e4f82787fa3e23
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2023-10-23 17:06:54 +0800

    Support OEM-META write file request for post code history
    Support write file req when multiple hosts send postcode to BMC
    and store its post code history.
    
    Change-Id: I56d303a5bc2f059451ee207a1de591be0d9993ac
    Signed-off-by: Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>

--- pldmd/pldmd.cpp
+++ pldmd/pldmd.cpp
@@ -65,6 +65,10 @@ PHOSPHOR_LOG2_USING;
 #include "oem_ibm.hpp"
 #endif
 
+#ifdef OEM_META
+#include "libpldmresponder/oem_meta_file_io.hpp"
+#endif
+
 constexpr const char* PLDMService = "xyz.openbmc_project.PLDM";
 
 using namespace pldm;
@@ -265,6 +269,12 @@ int main(int argc, char** argv)
         dbusToPLDMEventHandler = std::make_unique<DbusToPLDMEvent>(
             pldmTransport.getEventSource(), hostEID, instanceIdDb, &reqHandler);
     }
+
+#ifdef OEM_META
+    invoker.registerHandler(
+        PLDM_OEM, std::make_unique<pldm::responder::oem_meta::FileIOHandler>(
+                      configurationDiscovery.get()));
+#endif
 
     auto fruHandler = std::make_unique<fru::Handler>(
         FRU_JSONS_DIR, FRU_MASTER_JSON, pdrRepo.get(), entityTree.get(),
